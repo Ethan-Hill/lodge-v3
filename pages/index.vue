6847f936cf568ddd04ef787371ca1847686655d0
Ethan Hill <ethanhilljay@outlook.com>
2020-08-10 01:36:35 +0100

    Lodge

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLink to="/fun">Fun</NuxtLink>
+    <h1>Hello world!</h1>
+  </div>
+</template>
+
+<style scoped>
+    @import url(https://fonts.googleapis.com/css?family=Ubuntu+Mono:regular,italic,700,700italic);
+
+    * {
+        font-family: 'Ubuntu Mono';
+    }
+</style>
---
+++ pages/todos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ this.count }}</h1>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState([
+    'count'
+  ]),
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    }
+  },
+};
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
